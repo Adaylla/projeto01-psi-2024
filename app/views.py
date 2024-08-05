90e83aa3396f52fada1972d75ab319a3b87f140c
Adaylla <a.adaylla@escolar.ifrn.edu.br>
2024-08-05 19:16:27 -0300

    criando base.html e index.html

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def jogadores(request):
+    return render(request, "jogadores.html")
+
+def sobre(request):
+    return render(request, "sobre.html")
